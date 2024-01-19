3158b0564b9c32281e3149f3d9d3fb21b0a1b5dd
Melody Li <zli@lcs.on.ca>
2024-01-19 09:12:38 -0500

    Initial Commit

---
+++ TradingCards2/TradingCards2App.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCards2App.swift
+//  TradingCards2
+//
+//  Created by 利梓萌 on 2024-01-19.
+//
+
+import SwiftUI
+
+@main
+struct TradingCards2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
